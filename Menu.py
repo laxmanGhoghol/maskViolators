e179e05d7cee74cd085eb87c1612433875b38469
laxmanGhoghol <ghoghollaxman@gmail.com>
2021-04-24 12:38:06 +0530

    final completetion with test complete

---
+++ Menu.py
@@ -0,0 +1,18 @@
+print('\n [Welcome to face mask violation detection and recognition system.]')
+print('''
++-----------------+Menu+----------------+
+| 1. New User Entry                     |
+| 2. today's violators                  |
+| 3. Start Main Application             |
+| 4. Close/Exit                         |
++---------------------------------------+
+
+Note: change directory to project home directory.
+
+for choice 1 type this command in conda powershell: 'python NewUserEntry.py' after following instruction
+type this command 'python faceEncodings.py generate'
+
+for choice 2 type this command in conda powershell: 'python todayViolators.py' 
+
+for choice 1 type this command in conda powershell: 'python Main.py'
+''')
